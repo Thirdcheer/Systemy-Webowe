a6c6bfb71d2b27f51f965cdffee19b79b966433d
Michał Regulski <regulskimichal@outlook.com>
2017-12-21 03:01:08 +0100

    PHP3 login on sql database

--- login.php
+++ login.php
@@ -4,29 +4,48 @@ session_start();
 $_SESSION['logged'] = false;
 $_SESSION['logout'] = false;
 
-$creds = array();
-$creds['admin'] = 'admin';
-$creds['randomuser'] = 'random';
+$db = mysqli_connect("localhost", "root", "root", "default_schema");
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
 
 if (isset($_POST['submitLogin'])) {
-    $_SESSION['login'] = $_POST['login'];
-    $_SESSION['password'] = $_POST['password'];
+    $username = $_POST['login'];
+    $password = $_POST['password'];
 
-    if (array_key_exists($_SESSION['login'], $creds) && $creds[$_SESSION['login']] == $_SESSION['password']) {
-        $_SESSION['logged'] = true;
-        print('You have successfully logged in');
-        if(strpos($_SERVER['HTTP_REFERER'], 'not_logged.php') !== false){
-            header("Location: survey.php");
+    $escaped_username = $db->real_escape_string($username);
+
+    $query = "select * from user where username = '$escaped_username'";
+    $result = $db->query($query);
+    $db->close();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $hashed_password = $row['password'];
+
+        if (password_verify($password, $hashed_password)) {
+            $_SESSION['logged'] = true;
+            $_SESSION['login'] = $username;
+            $_SESSION['password'] = '';
+            $_SESSION['name'] = $row['name'];
+            $_SESSION['surname'] = $row['surname'];
+            $_SESSION['month_of_bith'] = $row['month_of_birth'];
+            $_SESSION['phone'] = $row['phone'];
+            $_SESSION['email'] = $row['email'];
+
+            echo 'You have successfully logged in';
+
+            if (strpos($_SERVER['HTTP_REFERER'], 'not_logged.php') !== false) {
+                header("Location: survey.php");
+            } else {
+                header("Location: {$_SERVER['HTTP_REFERER']}");
+            }
         }
-        else{
-            echo($_SERVER['HTTP_REFERER']);
-            header("Location: {$_SERVER['HTTP_REFERER']}");
-        }
-    } else {
-        $_SESSION['logged'] = false;
+    }
+
+    if (!$_SESSION['logged']) {
         print('Wrong login or password');
         header("Location: {$_SERVER['HTTP_REFERER']}");
     }
 }
-
-?>
--- sql/user.sql
+++ sql/user.sql
@@ -1,14 +1,18 @@
 CREATE SCHEMA default_schema;
 
 CREATE TABLE default_schema.User (
-  id       BIGINT                             NOT NULL AUTO_INCREMENT,
-  username VARCHAR(255) UNIQUE                NOT NULL,
-  password VARCHAR(64)                        NOT NULL,
-  email    VARCHAR(255) UNIQUE                NOT NULL,
+  id             BIGINT                             NOT NULL AUTO_INCREMENT,
+  username       VARCHAR(255) UNIQUE                NOT NULL,
+  name           VARCHAR(255),
+  surname        VARCHAR(255),
+  password       VARCHAR(60)                        NOT NULL,
+  email          VARCHAR(255) UNIQUE                NOT NULL,
+  phone          VARCHAR(255),
+  month_of_birth VARCHAR(255),
   PRIMARY KEY (id)
 );
 
-INSERT INTO User VALUES
-  (DEFAULT, 'admin', sha2('admin', 256), 'admin@artishere.com'),
-  (DEFAULT, 'admin2', sha2('admin2', 256), 'admin2@artishere.com'),
-  (DEFAULT, 'user', sha2('password', 256), 'user@gmail.com');
+INSERT INTO User (username, password, email) VALUES
+  ('admin', '$2y$10$.dOCk2xrhdKp.AD7NLcWje8po8v6sxzFkDzf/806zfmD2UmVYjkMm', 'admin@artishere.com'), #admin
+  ('admin2', '$2y$10$aXHjPZ1aEwZwHqQ/6CUYH.gu00hanrNRxJoVSFeMVwzaJWKp83rdO', 'admin2@artishere.com'), #admin2
+  ('user', '$2y$10$MbmbKCz75h1WyuPhpZMAjuIMzj3fYLJhxuS861tKYA40qTF3iCTJa', 'user@gmail.com'); #password
